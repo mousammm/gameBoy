d2a7b56ab027c3f7eb469c071fbb29ed64d57ff9
mousammm <mxmssdia@gmail.com>
2025-12-25 13:34:12 +0530

    get argument

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc,char **argv)
 {
-	printf("Hello world!\n");
+	if(argc < 2) {
+		printf("usage gb <path/to/rom>\n");
+		return 0;
+	}
+	printf("Hello world! %s %s\n", argv[0], argv[1]);
 	return 0;
 }
