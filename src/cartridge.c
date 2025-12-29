a507163d953af396c14f8f74dada9c725fe192e1
mousammm <mxmssdia@gmail.com>
2025-12-30 02:00:13 +0530

    add: header parse

--- src/cartridge.c
+++ src/cartridge.c
@@ -42,17 +42,38 @@ Cartridge* load_rom(const char* filename)
     // Parse ROM headers
     // TITLE (16char max, but some have 15 or 11)
     int title_length = 16;
-    // check for early termination
-    for (int i = 0; i < 16; i++) {
-        uint8_t c = cart->rom_data[HEADER_TITLE + i];
-        if (c == 0x00 || c == 0x80 || c == 0xC0) {
-            title_length = i;
-            break;
-        }
-    }
-    
     memcpy(cart->title, &cart->rom_data[HEADER_TITLE], title_length);
     cart->title[title_length] = '\0';
+
+    // Cartridge type
+    cart->cartridge_type = cart->rom_data[HEADER_CARTIDGE_TYPE];
+    // TODO: Check if Cartridge has ram and battery
+
+    // ROM size
+    uint8_t rom_size_code = cart->rom_data[HEADER_ROM_SIZE];
+    if (rom_size_code < sizeof(ROM_SIZES)/sizeof(ROM_SIZES[0])) {
+        cart->rom_banks = (1 << (rom_size_code + 1));
+    } else {
+        cart->rom_banks = 2; // Default to 32kb
+    }
+
+    // RAM size
+    uint8_t ram_size_code = cart->rom_data[HEADER_RAM_SIZE];
+    if (ram_size_code < sizeof(RAM_SIZES)/sizeof(RAM_SIZES[0])) {
+        cart->ram_size = ROM_SIZES[ram_size_code];
+        if (cart->ram_size > 0) {
+            cart->ram_banks = cart->ram_size / RAM_BANK_SIZE;
+            cart->ram_data = (uint8_t*)calloc(cart->ram_size, 1);
+            // TODO: !cart->ram_data && cart->ram_size > 0
+        }
+    }
+
+    // Destination code 
+    cart->destination_code = cart->rom_data[HEADER_DESTINATION_CODE];
+    // Old license code 
+    cart->old_licensee_code = cart->rom_data[HEADER_OLD_LICENSE_CODE];
+    // MASK rom version
+    cart->mask_rom_version = cart->rom_data[HEADER_MASK_ROM_VERSION];
 
     return cart;
 }
@@ -60,4 +81,18 @@ Cartridge* load_rom(const char* filename)
 void print_rom_info(Cartridge *cart)
 {
     printf("Title: %s\n", cart->title);
+    printf("ROM Size: %u bytes (%d banks)\n", cart->rom_size, cart->rom_banks);
+    printf("RAM Size: %u bytes (%d banks)\n", cart->ram_size, cart->ram_banks);
+
+    // Destination code
+    const char* destination_str = "Unknown";
+    switch(cart->destination_code) {
+        case 0x00: destination_str = "Japanese"; break;
+        case 0x01: destination_str = "Non-Japanese"; break;
+    }
+    printf("Destination: %s\n", destination_str);
+
+    // Old licensee code (common ones)
+    printf("Old Licensee Code: 0x%02X\n", cart->old_licensee_code);
+    printf("Mask ROM Version: %d\n", cart->mask_rom_version);
 }
